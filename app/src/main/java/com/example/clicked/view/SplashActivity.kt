117719e199abc179df958377f6ad1edd51f1a961
Zahrina Candrakanti <zahrinacandrakanti@gmail.com>
2024-05-31 20:39:00 +0700

    Welcome Page

--- app/src/main/java/com/example/clicked/view/SplashActivity.kt
+++ app/src/main/java/com/example/clicked/view/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.clicked.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.clicked.R
 import kotlinx.coroutines.CoroutineScope
@@ -15,11 +16,15 @@ class SplashActivity : AppCompatActivity() { // Use consistent naming
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
         val scope = CoroutineScope(Dispatchers.IO) // Launch coroutine in background
 
         scope.launch { // Use coroutine for non-UI tasks
             delay(3000L) // Delay for 3 seconds
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            val intent = Intent(this@SplashActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
         }
